76aed735cfeeb3217b74a7754e9c447e836d64b1
LemmyOnukwugha <114013405+LemmyOnukwugha@users.noreply.github.com>
2023-06-07 14:34:31 -0400

    contains partially finished login page design

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -10,20 +10,22 @@ export default function LoginPage() {
         <div className={styles.logo}>
           <h1>Euphoric Encounters</h1>
         </div>
-        <div className={styles.login_cta}>
-          <h2>Login Below</h2>
-          <Button>
-            <FcGoogle size={25} className={styles.icon} />
-            Login With Google
-          </Button>
-          <Button>
-            <BsApple size={25} className={styles.icon} color={"#000000"} />
-            Login With Apple
-          </Button>
-          <Button>
-            <BsFacebook size={25} color={"#4267B2"} className={styles.icon} />
-            Login With FaceBook
-          </Button>
+        <div className={styles.login_cta_container}>
+          <div className={styles.login_cta}>
+            <h2>Login Below</h2>
+            <Button>
+              <FcGoogle size={25} className={styles.icon} />
+              Login With Google
+            </Button>
+            <Button>
+              <BsApple size={25} className={styles.icon} color={"#000000"} />
+              Login With Apple
+            </Button>
+            <Button>
+              <BsFacebook size={25} color={"#4267B2"} className={styles.icon} />
+              Login With FaceBook
+            </Button>
+          </div>
         </div>
       </div>
     </div>
